cbabdec786d9c84c32028a48974fb6a7a8499b78
ptruffault <ptruffault@hotmail.com>
2018-06-07 13:05:37 +0200

    [make save]

--- SRCS/IS/ft_ismaj.c
+++
@@ -1,9 +0,0 @@
-#include "../../includes/libft.h"
-
-int	ft_ismaj(char c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
--- SRCS/IS/ft_ismin.c
+++
@@ -1,9 +0,0 @@
-#include "../../includes/libft.h"
-
-int	ft_ismin(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
