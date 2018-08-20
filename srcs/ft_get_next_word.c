18ebebdbeb99689d53814ce3eb2b1f75ce37a7dc
Paul TRUFFAULT <ptruffau@e1r8p14.42.fr>
2018-08-20 14:37:20 +0200

    [make save]

--- srcs/ft_get_next_word.c
+++ srcs/ft_get_next_word.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_word.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptruffau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/21 14:49:50 by ptruffau          #+#    #+#             */
+/*   Updated: 2018/06/21 14:50:01 by ptruffau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/libft.h"
 
 char		*ft_get_next_word(char *str)
@@ -19,4 +31,4 @@ char		*ft_get_next_word(char *str)
 		word = ft_strncpy(word, str, j);
 	}
 	return (word);
-} 
+}
