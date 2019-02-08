fa42162d64a7163e819113ce31be694577aac1a6
Alexandre DI ROSA <adi-rosa@e1r3p7.42.fr>
2019-02-08 14:54:22 +0100

    done

--- srcs/ft_write_in_file.c
+++ srcs/ft_write_in_file.c
@@ -6,7 +6,7 @@
 /*   By: ptruffau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/13 15:27:04 by ptruffau          #+#    #+#             */
-/*   Updated: 2019/02/05 13:46:17 by adi-rosa         ###   ########.fr       */
+/*   Updated: 2019/02/08 14:53:52 by adi-rosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	ft_write_in_file(char *path, char *str)
 {
 	int fd;
 
-	if ((fd = open(path, O_RDWR | O_CREAT | O_APPEND, S_IRWXU | S_IRWXG | S_IRWXO)) < 0)
+	if ((fd = open(path, O_RDWR | O_CREAT | O_APPEND
+					, S_IRWXU | S_IRWXG | S_IRWXO)) < 0)
 		return ;
 	ft_putendl_fd(str, fd);
 	ft_close(fd);
